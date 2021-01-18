ded479a763b49aaf5957408e8405cd80ce87df88
david.guerrero <dguerrero_bcn@hotmail.com>
2021-01-19 00:34:57 +0100

    Finishing tests for validate the behaviour of the stimulus from memory to predicate depending on the behaviour of the stimulus created.

--- goap/AgentWithSpecificStimulusCasesTest.cpp
+++ goap/AgentWithSpecificStimulusCasesTest.cpp
@@ -17,6 +17,27 @@
 using namespace NAI::Goap;
 using ::testing::NiceMock;
 
+
+class PermanentPredicate : public BasePredicate
+{
+public:
+	PermanentPredicate() : BasePredicate("PermanentPredicate") {}
+	virtual ~PermanentPredicate() = default;
+};
+
+class DynamicPredicate : public BasePredicate
+{
+public:
+	DynamicPredicate(int v) : BasePredicate("DynamicPredicate"), mValue{v} {}
+	virtual ~DynamicPredicate() = default;
+
+	int GetValue() const { return mValue; }
+	void SetValue(int v) { mValue = v; }
+
+private:
+	int mValue;	
+};
+
 class AcceptAllThreshold : public IThreshold
 {
 public:
@@ -42,12 +63,53 @@ public:
 	unsigned GetId() const override { return 1; }
 };
 
+class PermanentStimulus : public IStimulus
+{
+public:
+	PermanentStimulus() = default;
+	virtual ~PermanentStimulus() = default;
+
+	std::string GetClassName() const override { return typeid(PermanentStimulus).name(); }
+	glm::vec3 GetPosition() const override { return glm::vec3(1.0f); }
+	float GetDurationInMemory() const override { return 60.0f; }
+	unsigned GetId() const override { return 1; }
+};
+
+class DynamicStimulus : public IStimulus
+{
+public:
+	DynamicStimulus(int v) : mValue{v} {}
+	virtual ~DynamicStimulus() = default;
+
+	std::string GetClassName() const override { return typeid(DynamicStimulus).name(); }
+	glm::vec3 GetPosition() const override { return glm::vec3(1.0f); }
+	float GetDurationInMemory() const override { return 60.0f; }
+	unsigned GetId() const override { return 1; }
+	int GetValue() const { return mValue; }
+
+private:
+	int mValue;
+};
+
 class InstantStimulusSensor : public BaseSensor
 {
 public:
 	InstantStimulusSensor() = default;
-	virtual ~InstantStimulusSensor() = default;
-	
+	virtual ~InstantStimulusSensor() = default;	
+};
+
+class PermanentStimulusSensor : public BaseSensor
+{
+public:
+	PermanentStimulusSensor() = default;
+	virtual ~PermanentStimulusSensor() = default;	
+};
+
+class DynamicStimulusSensor : public BaseSensor
+{
+public:
+	DynamicStimulusSensor() = default;
+	virtual ~DynamicStimulusSensor() = default;	
 };
 
 class InstantStimulusGoal : public BaseGoal
@@ -56,25 +118,72 @@ public:
 	InstantStimulusGoal() = default;
 	virtual ~InstantStimulusGoal() = default;
 
-
 protected:
 	std::shared_ptr<IPredicate>
 	DoTransformStimulusIntoPredicates(const ShortTermMemory<IStimulus>& memory) const override
 	{
+		assert(memory.GetSize() <= 1);
+		
+		std::shared_ptr<IPredicate> predicate = nullptr;
+		
 		memory.PerformActionForEach(
-			[](std::shared_ptr<IStimulus> stimulus) -> bool
+			[&predicate](std::shared_ptr<IStimulus> stimulus) -> bool
 			{
+				predicate =  std::make_shared<BasePredicate>("InstantPredicate");
 				return true;
 			});
 
-		if(!memory.IsEmpty())
-		{
-			return std::make_shared<BasePredicate>("InstantStimulusPredicate");
-		}
-		else
-		{
-			return nullptr;
-		}
+		return predicate;
+	}
+};
+
+class PermanentStimulusGoal : public BaseGoal
+{
+public:
+	PermanentStimulusGoal() = default;
+	virtual ~PermanentStimulusGoal() = default;
+
+protected:
+	std::shared_ptr<IPredicate>
+    DoTransformStimulusIntoPredicates(const ShortTermMemory<IStimulus>& memory) const override
+	{
+		assert(memory.GetSize() <= 1);
+		std::shared_ptr<IPredicate> predicate = nullptr;
+		
+		memory.PerformActionForEach(
+            [&predicate](std::shared_ptr<IStimulus> stimulus) -> bool
+            {
+                predicate = std::make_shared<PermanentPredicate>();
+                return true;
+            });
+
+		return predicate;
+	}
+};
+
+class DynamicStimulusGoal : public BaseGoal
+{
+public:
+	DynamicStimulusGoal() = default;
+	virtual ~DynamicStimulusGoal() = default;
+
+protected:
+	std::shared_ptr<IPredicate>
+    DoTransformStimulusIntoPredicates(const ShortTermMemory<IStimulus>& memory) const override
+	{
+		assert(memory.GetSize() <= 1);
+		std::shared_ptr<IPredicate> predicate = nullptr;
+		
+		memory.PerformActionForEach(
+            [&predicate](std::shared_ptr<IStimulus> stimulus) -> bool
+            {
+            	const auto dynamicStimulus = std::static_pointer_cast<DynamicStimulus>(stimulus);
+                predicate = std::make_shared<DynamicPredicate>(dynamicStimulus->GetValue());
+            	
+                return true;
+            });
+
+		return predicate;
 	}
 };
 
@@ -137,51 +246,149 @@ TEST(NAI_AgentWithSpecificStimulusCases, When_InstantStimulus_Then_It_RemainsDur
 		agent->Update(0.016f);
 		ASSERT_TRUE(sensorySystem->GetReceivedStimulus().size() == 0);
 		ASSERT_TRUE(perceptionSystem->GetMemory().GetSize() == 1);
+		ASSERT_TRUE(agent->GetPredicates().size() == 1);
 	}
-	
-	ASSERT_TRUE(agent->GetPredicates().size() == 1);
 }
 
-TEST(NAI_AgentWithSpecificStimulusCases, When_InstantStimulus_Then_OnePredicateAdded) 
-{
-	
-}
-
-//Stimulus of something that happens and will never change, it remains the same forever, like a the agent
-//see a Building.
+//Stimulus of something that happens and will never change, it remains the same forever, like a Building.
 //This stimulus will be in memory for a while, if its seen again then will be updated in memory.
 //The predicate created if the case will be always the same.
 
-TEST(NAI_AgentWithSpecificStimulusCases, When_StimulusOfStaticPermanentThing_Then_It_RemainsDuringATime) 
+TEST(NAI_AgentWithSpecificStimulusCases, When_StimulusOfStaticPermanentThingWhichAlreadyExists_Then_PredicateIsAlwaysTheSame) 
 {
+	const auto sensorySystem = std::make_shared<SensorySystem<IStimulus>>();
+	PermanentStimulusSensor permanentStimulusSensor;
 	
-}
+	permanentStimulusSensor.Subscribe(sensorySystem);
 
-TEST(NAI_AgentWithSpecificStimulusCases, When_StimulusOfStaticPermanentThing_Then_OnePredicateAdded) 
-{
-	
-}
+	const auto perceptionSystem = std::make_shared<NiceMock<MyPerceptionSystemMock>>(sensorySystem);
 
-TEST(NAI_AgentWithSpecificStimulusCases, When_StimulusOfStaticPermanentThingWhichAlreadyExists_Then_PredicateIsUpdated) 
-{
-	
+	std::vector<std::shared_ptr<IGoal>> goals = {std::make_shared<PermanentStimulusGoal>()};
+	std::vector<std::shared_ptr<IPredicate>> predicates;
+	auto agent = std::make_shared<MyAgent>(
+        std::make_shared<TreeGoapPlanner>(),
+        goals,
+        predicates,
+        perceptionSystem);
+
+	agent->AddSensoryThreshold(typeid(PermanentStimulus).name(), std::make_shared<AcceptAllThreshold>());
+
+	permanentStimulusSensor.NotifyAll(std::make_shared<PermanentStimulus>());
+	agent->StartUp();
+	agent->Update(0.016f);
+	for(auto i = 0; i < 10; ++i)
+	{
+		agent->Update(0.016f);
+		permanentStimulusSensor.NotifyAll(std::make_shared<PermanentStimulus>());
+		ASSERT_TRUE(sensorySystem->GetReceivedStimulus().size() == 1);
+		ASSERT_TRUE(perceptionSystem->GetMemory().GetSize() == 1);
+		ASSERT_TRUE(agent->GetPredicates().size() == 1);
+	}
 }
 
 //Stimulus of something that can be changing during time. For instance, another agent.
 //We want to remember once it disappears as stimulus for a while.
 //The predicate created if the case will be updated everytime, will be always the same
 
-TEST(NAI_AgentWithSpecificStimulusCases, When_StimulusOfDinamicThing_Then_It_RemainsDuringATime) 
-{
-	
-}
-
-TEST(NAI_AgentWithSpecificStimulusCases, When_StimulusOfDinamicThing_Then_OnePredicateAdded) 
-{
-	
-}
-
 TEST(NAI_AgentWithSpecificStimulusCases, When_StimulusOfDinamicThingWhichAlreadyExists_Then_PredicateIsUpdated) 
 {
+	const auto sensorySystem = std::make_shared<SensorySystem<IStimulus>>();
+	DynamicStimulusSensor dynamicStimulusSensor;
 	
+	dynamicStimulusSensor.Subscribe(sensorySystem);
+
+	const auto perceptionSystem = std::make_shared<NiceMock<MyPerceptionSystemMock>>(sensorySystem);
+
+	std::vector<std::shared_ptr<IGoal>> goals = {std::make_shared<DynamicStimulusGoal>()};
+	std::vector<std::shared_ptr<IPredicate>> predicates;
+	auto agent = std::make_shared<MyAgent>(
+        std::make_shared<TreeGoapPlanner>(),
+        goals,
+        predicates,
+        perceptionSystem);
+
+	agent->AddSensoryThreshold(typeid(DynamicStimulus).name(), std::make_shared<AcceptAllThreshold>());
+
+	dynamicStimulusSensor.NotifyAll(std::make_shared<DynamicStimulus>(0));
+	agent->StartUp();
+	agent->Update(0.016f);
+	for(auto i = 0; i < 10; ++i)
+	{
+		agent->Update(0.016f);
+		dynamicStimulusSensor.NotifyAll(std::make_shared<DynamicStimulus>(i+1));
+		ASSERT_TRUE(sensorySystem->GetReceivedStimulus().size() == 1);
+		ASSERT_TRUE(perceptionSystem->GetMemory().GetSize() == 1);
+		perceptionSystem->GetMemory().PerformActionForEach(
+			[i](std::shared_ptr<IStimulus> stimulus)-> bool
+			{
+				auto dynamicStimulus = std::static_pointer_cast<DynamicStimulus>(stimulus);
+				EXPECT_TRUE(dynamicStimulus->GetValue() == i);
+				return true;
+			});
+		ASSERT_TRUE(agent->GetPredicates().size() == 1);
+		auto dynamicPredicate = std::static_pointer_cast<DynamicPredicate>(agent->GetPredicates()[0]);
+		ASSERT_TRUE(dynamicPredicate->GetValue() == i);
+	}
+}
+
+TEST(NAI_AgentWithSpecificStimulusCases, When_StimulusOfDinamicThingDisappearsAfterAWhile_Then_PredicateRemainsForEverWithTheLastValueUpdated) 
+{
+	const auto sensorySystem = std::make_shared<SensorySystem<IStimulus>>();
+	DynamicStimulusSensor dynamicStimulusSensor;
+	
+	dynamicStimulusSensor.Subscribe(sensorySystem);
+
+	const auto perceptionSystem = std::make_shared<NiceMock<MyPerceptionSystemMock>>(sensorySystem);
+
+	std::vector<std::shared_ptr<IGoal>> goals = {std::make_shared<DynamicStimulusGoal>()};
+	std::vector<std::shared_ptr<IPredicate>> predicates;
+	auto agent = std::make_shared<MyAgent>(
+        std::make_shared<TreeGoapPlanner>(),
+        goals,
+        predicates,
+        perceptionSystem);
+
+	agent->AddSensoryThreshold(typeid(DynamicStimulus).name(), std::make_shared<AcceptAllThreshold>());
+
+	dynamicStimulusSensor.NotifyAll(std::make_shared<DynamicStimulus>(0));
+	agent->StartUp();
+	agent->Update(1.0f);
+	for(auto i = 0; i < 10; ++i)
+	{
+		agent->Update(1.0f);
+		dynamicStimulusSensor.NotifyAll(std::make_shared<DynamicStimulus>(i+1));
+	}
+	
+	perceptionSystem->GetMemory().PerformActionForEach(
+            [](std::shared_ptr<IStimulus> stimulus)-> bool
+            {
+                auto dynamicStimulus = std::static_pointer_cast<DynamicStimulus>(stimulus);
+                EXPECT_TRUE(dynamicStimulus->GetValue() == 9);
+                return true;
+            });
+	auto dynamicPredicate = std::static_pointer_cast<DynamicPredicate>(agent->GetPredicates()[0]);
+	ASSERT_TRUE(dynamicPredicate->GetValue() == 9);
+
+	//Now, the dynamic stimulus disappears, and remains in the memory for a while with the same value.
+	for(auto i = 0; i < 59; ++i)
+	{
+		//the first frame it has the last new stimulus with 10.
+		agent->Update(1.0f);
+		ASSERT_TRUE(sensorySystem->GetReceivedStimulus().size() == 0);
+		ASSERT_TRUE(perceptionSystem->GetMemory().GetSize() == 1);
+		perceptionSystem->GetMemory().PerformActionForEach(
+            [i](std::shared_ptr<IStimulus> stimulus)-> bool
+            {
+                auto dynamicStimulus = std::static_pointer_cast<DynamicStimulus>(stimulus);
+                EXPECT_TRUE(dynamicStimulus->GetValue() == 10);
+                return true;
+            });
+		ASSERT_TRUE(agent->GetPredicates().size() == 1);
+		auto dynamicPredicate = std::static_pointer_cast<DynamicPredicate>(agent->GetPredicates()[0]);
+		ASSERT_TRUE(dynamicPredicate->GetValue() == 10);
+	}
+
+	agent->Update(1.0f);
+	ASSERT_TRUE(perceptionSystem->GetMemory().GetSize() == 0);
+	ASSERT_TRUE(agent->GetPredicates().size() == 1);
 }
